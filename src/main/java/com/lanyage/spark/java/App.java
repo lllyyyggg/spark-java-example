cc560875dc15a1afd0c4e4ab9bf63288f8f881fc
lllyyyggg <962221804@qq.com>
2019-03-22 20:45:22 +0800

    initial commit

---
+++ src/main/java/com/lanyage/spark/java/App.java
@@ -0,0 +1,40 @@
+package com.lanyage.spark.java;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+
+        SparkSession session = SparkSession
+                .builder()
+                .appName("SparkJavaExample")
+                .master("local[3]")     //表示使用本地的3个线程
+                .getOrCreate();
+
+
+        try (JavaSparkContext context = new JavaSparkContext(session.sparkContext())) {
+            List<Integer> integers = Arrays.asList(1, 4, 5, 6, 7, 8, 9
+                    , 10, 11, 12, 13, 14, 15);
+
+            JavaRDD<Integer> javaRDD = context.parallelize(integers, 3);
+
+            javaRDD.foreach(integer -> {
+                System.out.println("Java RDD:" + integer);
+                Thread.sleep(3000);
+            });
+
+            Thread.sleep(1000000);
+            context.stop();
+        }
+
+
+    }
+}
